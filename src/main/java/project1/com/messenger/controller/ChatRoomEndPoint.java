1400d7f05ec0eae3708e6c09ca73fdcfa72dfbc0
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2020-01-01 11:30:27 +0700

    websocket

--- src/main/java/project1/com/messenger/controller/ChatRoomEndPoint.java
+++ src/main/java/project1/com/messenger/controller/ChatRoomEndPoint.java
@@ -12,35 +12,26 @@ import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint(value = "/chatRoomServer")
 public class ChatRoomEndPoint {
-	 static Set<Session> users = Collections.synchronizedSet(new HashSet<Session>());
+	static Set<Session> users = Collections.synchronizedSet(new HashSet<Session>());
 	  @OnOpen
-	  public void handleOpen(Session session) throws IOException {
-	    session.getBasicRemote().sendText("You are connected as: " + session.getId());
+	  public void handleOpen(Session session) {
 	    users.add(session);
-	    sendListUserOnline();
 	  }
 	  @OnMessage
 	  public void handleMessage(String message, Session userSession) throws IOException {
-	    for (Session session : users) {
-	      session.getBasicRemote().sendText(userSession.getId() + ": " + message);
+	    String username = (String) userSession.getUserProperties().get("username");
+	    if (username == null) {
+	      userSession.getUserProperties().put("username", message);
+	      userSession.getBasicRemote().sendText("System: you are connectd as" + message);
+	    } else {
+	      for (Session session : users) {
+	        session.getBasicRemote().sendText(username + ": " + message);
+	      }
 	    }
-	  }
-	  public void sendListUserOnline() throws IOException {
-	    for (Session session : users) {
-	      session.getBasicRemote().sendText(buildListUser());
-	    }
-	  }
-	  public String buildListUser() {
-	    StringBuffer listUser = new StringBuffer("list_user");
-	    for (Session session : users) {
-	      listUser.append(session.getId() + " \n");
-	    }
-	    return listUser.toString();
 	  }
 	  @OnClose
-	  public void handleClose(Session session) throws IOException {
+	  public void handleClose(Session session) {
 	    users.remove(session);
-	    sendListUserOnline();
 	  }
 	  @OnError
 	  public void handleError(Throwable t) {
